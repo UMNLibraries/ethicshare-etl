798af4905ea30f81a0f56373886509b865f8998d
David Naughton <nihiliad@gmail.com>
2015-10-22 16:19:12 -0500

    Fixed a missing-parameter bug in most of the cron/*.php apps.

--- cron/080-waposthealth.php
+++ cron/080-waposthealth.php
@@ -5,7 +5,7 @@ $binDir = realpath(dirname(__FILE__) . '/../bin');
 $basename = basename(__FILE__, '.php');
 $source = preg_replace('/^\d+-/', '', $basename);
 
-exec($binDir . '/etl-source-full-cycle.php 2>&1', $output, $return);
+exec($binDir . '/etl-source-full-cycle.php --source=' . $source . ' 2>&1', $output, $return);
 if ($return) {
   $error = array_pop($output);
   exit("System call '$cmd' failed: '$error'\n");
